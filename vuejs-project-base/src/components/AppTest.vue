10c55a71974e00f33d94c5230d088898bc32a9a8
Yaroslav-Pahomov <pahomstyle90@mail.ru>
2023-09-07 13:12:56 +0500

    Объект data Vue, переменные

--- vuejs-project-base/src/components/AppTest.vue
+++ vuejs-project-base/src/components/AppTest.vue
@@ -1,22 +1,39 @@
 <script>
 export default {
   name: 'AppTest',
+
+  // Переменные
+  data () {
+    return {
+      name: 'John',
+      surname: 'Smith',
+    }
+  },
+
+
 }
 </script>
 
 <template>
-  <div>
+  <div class="container">
     <h2 class="green">
       Hello, World AppTest!
     </h2>
-    <p>
-      app-test app-test app-test app-test app-test app-test app-test app-test
-    </p>
+    <p>{{ name }}</p>
+    <p>{{ surname }}</p>
   </div>
 </template>
 
 <style scoped>
+div.container {
+  margin-top: 40px;
+  margin-bottom: 40px;
+}
+
 h2 {
+  text-align: center;
+}
+p {
   text-align: center;
 }
 </style>
